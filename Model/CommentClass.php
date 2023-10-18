2871ffc3c0ff15309e74aacae2dc000a7a5b219e
josemanuelcordon <josemanuelcordoncastillo01@gmail.com>
2023-10-18 19:46:28 +0200

    add post comments (must fix)

--- Model/CommentClass.php
+++ Model/CommentClass.php
@@ -5,6 +5,7 @@ class Comment
     private $text;
     private $article;
     private $author;
+    private $answers;
 
     public function __construct($datos)
     {
@@ -12,6 +13,7 @@ class Comment
         $this->text = $datos['comentario'];
         $this->article = ArticleRepository::getArticleById($datos['articulo']);
         $this->author = UserRepository::getUserById($datos['autor']);
+        $this->answers = CommentRepository::getAnswers($this->id);
     }
 
     public function getId()
@@ -34,6 +36,11 @@ class Comment
         return $this->article;
     }
 
+    public function getAnswers()
+    {
+        return $this->answers;
+    }
+
     public function setId($iden)
     {
         return $this->id = $iden;
@@ -52,5 +59,25 @@ class Comment
     public function setArtile($article)
     {
         return $this->article = $article;
+    }
+
+    public function showAnswers($idArticulo, $nivel = 1)
+    {
+        foreach ($this->answers as $answer) {
+            if (empty(CommentRepository::getAnswers($answer->getId()))) {
+                echo '<article style="padding-left:' . ($nivel * 5) . ';" id="comment-answer">';
+                echo '<p style="color:black;">' . $answer->getAuthor()->getUserName() . '</p>';
+                echo '<p>' . $answer->getComment() . '</p>';
+                echo '</article>';
+                echo '<form id="answer-form" action="" method="post">
+                <textarea rows="3" name="comment" id="answer-send"></textarea>
+                <input type="hidden" name="id_comentario" value="' . $answer->getId() . '"/>
+                <input type="hidden" name="id_articulo" value="' . $idArticulo . '"/>
+                <input id="answer-submit" type="submit" value="send-comment"/>
+                </form>';
+            } else {
+                $answer->showAnswers($idArticulo, ++$nivel);
+            }
+        }
     }
 }
---
+++ View/main.js
@@ -0,0 +1,5 @@
+const answerButton = document.getElementById("answer-button");
+
+const handleAnswerButton = () => {};
+
+answerButton.addEventListener("click", handleAnswerButton);
